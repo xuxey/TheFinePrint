eef11305d9ed1721f44856c5cdf4e37e98d0b896
animeshsingh98 <animesh8@illinois.edu>
2024-02-24 00:44:15 -0600

    Working backend

--- backend/service.py
+++ backend/service.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 from llm_query.prompts import terms_conditions
-import llm_query.gpt_query
+from llm_query.gpt_query import *
 import threading
 from pymongo import MongoClient
 
@@ -9,34 +9,34 @@ from pymongo import MongoClient
 # else, create row and start llm process in background.
 # also, return row as json
 
+def function_caller(db, url):
+    query_engine = LLM_query()
+    query_engine.request(db, url)
 
 def summarise(db, url, input_file_name):
-
+    # get the results in db if any
     query_result = db.find_one({"url": url})
     if query_result != None:
-        if query_result["timestamp"] < datetime.now() - timedelta(days=1):
+        # validate if the file is a day old
+        if query_result["timestamp"] >= datetime.now() - timedelta(days=1):
             status = query_result["status"]
             output_file = query_result["output_file"]
             return {"status": status, "output_file": output_file}
         else:
-            result = db.update_one(
-                {"url": url}, {"$set": {"timestamp": datetime.now(), "status": "pending"}})
-            return {"status": "pending", "output_file": 'out_' + input_file_name}
+            result = db.update_one({"url": url}, {"$set": {"timestamp": datetime.now(), "status": "pending", "output_file": ""}})
+            threading.Thread(target=function_caller, args=[db, url]).start()
+            return {"status": "pending", "output_file": ''}    
 
     new_row = {
         "url": url,
         "timestamp": datetime.now(),
         "status": "progress",
         "input_file": input_file_name,
-        "output_file": 'out_' + input_file_name,
+        "output_file": "",
     }
     insert_result = db.insert_one(new_row)
-
-    query_engine = llm_query.gpt_query()
-    background_thread_for_query = threading.Thread(
-        target=query_engine.request(db, url))
-
-    return {"status": "pending", "output_file": ''}
+    threading.Thread(target=function_caller, args=[db, url]).start()
+    return {"status": "pending", "output_file": ''}    
 
 # return db entry if exists, or None
 
