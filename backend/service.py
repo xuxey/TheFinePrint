585f4ce655147716c2cbdbe5c3cad79a49169262
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2024-02-23 21:34:39 -0600

    init

---
+++ backend/service.py
@@ -0,0 +1,12 @@
+
+# check db if entry for url exists,
+# if exists, send row as json
+# else, create row and start llm process in background.
+# also, return row as json
+def summarise(db, url) -> dict:
+    pass
+
+
+# return db entry if exists, or None
+def get_summary(db, url) -> dict | None:
+    pass
