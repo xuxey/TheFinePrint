be5190e0e2d450f3a09a47391bf453753ba11861
Soham Kulkarni <sohamkrox@gmail.com>
2024-02-24 22:31:22 -0600

    update hero text

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -6,8 +6,8 @@
 <span class="flex flex-col md:flex-row h-screen items-center -mt-28 py-20">
 	<div class="flex flex-col w-2/3">
 		<span class="flex flex-row text-6xl font-sans gap-4">
-			Take Control Of Your
-			<div class="text-blue-700">Privacy</div>
+			Know Your
+			<div class="text-blue-700">Fine Print</div>
 		</span>
 		<div class="mt-3 text-2xl">
 			Until now, long drawn out legal documents to keep you in the dark.
@@ -15,7 +15,7 @@
 			<br />
 			With
 			<span class="font-cursive text-4xl text-blue-900">the fine print</span>, understand what
-			you're signing up for with on-demand AI Assisted summaries and warnings.
+			you're signing up for with on-demand AI Assisted summaries and insights.
 			<br />
 		</div>
 	</div>
